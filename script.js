875aab1904e355c90f546656458266bf72545904
Jakob Skote <jakob@untold.garden>
2024-05-29 21:42:11 +0200

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+function startAudio() {
+    const audioPlayer = document.getElementById('audioPlayer');
+    const startButton = document.getElementById('startButton');
+    const nextChapterButton = document.getElementById('nextChapter');
+
+    startButton.hidden = true;
+    audioPlayer.hidden = false;
+    audioPlayer.play();
+
+    audioPlayer.addEventListener('timeupdate', () => {
+        if (audioPlayer.duration - audioPlayer.currentTime <= 30) {
+            nextChapterButton.hidden = false;
+            nextChapterButton.onclick = function() {
+                window.location.href = 'https://your-external-link.com';
+            };
+        }
+    });
+}
