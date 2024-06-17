028ee6246b1c0562f0adac98f139f737698d1e80
Jakob Skote <jakob@untold.garden>
2024-06-17 17:56:31 +0200

    updated link

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function startAudio() {
         if (audioPlayer.duration - audioPlayer.currentTime <= 30) {
             nextChapterButton.hidden = false;
             nextChapterButton.onclick = function() {
-                window.location.href = 'https://app.meadow.space/e/-NssClTdGYR6Vy9tu3YT';
+                window.location.href = 'https://app.meadow.space/e/-O-al8Rovo0Q7feMHwuZ?i';
             };
         }
     });
